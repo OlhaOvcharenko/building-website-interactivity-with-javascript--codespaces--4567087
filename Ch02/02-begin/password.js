b80f11dabff8448188677d38812332579aa5a315
OlhaOvcharenko <134648751+OlhaOvcharenko@users.noreply.github.com>
2026-03-15 16:27:55 +0000

    add btnReveal to index.html and password

--- Ch02/02-begin/password.js
+++ Ch02/02-begin/password.js
@@ -1,5 +1,6 @@
-"use strict";
+'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    
-})
+  const btnReveal = document.querySelector('#reveal-password');
+  const fldPassword = document.querySelector('#txt-password');
+});
